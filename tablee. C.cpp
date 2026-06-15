a4a7a300512e1c4f26b24bdced48b0a27d6cfa93
shravanigholap021-max <shravanigholap021@gmail.com>
2026-06-15 12:10:29 +0530

    Add files via upload

---
+++ tablee. C.cpp
@@ -0,0 +1,14 @@
+//Name: shravani rajkumar gholap
+//PRN:2503033111372032
+#include <stdio.h>
+int main(){
+    int i,table,num;
+    printf("Enter A Number: ");
+    scanf("%d",&num);
+    for(i=1;i<=10;i++){
+        table=i*num;
+        printf("%d\n",table);
+    }
+    return 0;
+}
+   
