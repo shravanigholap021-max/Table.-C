30f785da4463bae366f4fa951199022b25e8b25e
shravanigholap021-max <shravanigholap021@gmail.com>
2026-06-27 16:43:25 +0530

    Add files via upload

---
+++ factorial. C.cpp
@@ -0,0 +1,13 @@
+//Name: shravani rajkumar gholap
+//PRN:2503033111372032
+#include<stdio.h>
+int main (){
+ int i=1,n,fact=1;
+ printf("Enter number: ");
+ scanf("%d",&n);
+ for ( i = 1; i<=n ; i++){
+    fact=fact*i;
+ }
+ printf("Factorial: %d",fact);
+return 0;
+}
