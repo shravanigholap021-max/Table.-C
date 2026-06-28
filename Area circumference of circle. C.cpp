ce9df4ac027e40efc932018e0cdc246f43723ed2
shravanigholap021-max <shravanigholap021@gmail.com>
2026-06-28 18:26:59 +0530

    Add files via upload

---
+++ Area circumference of circle. C.cpp
@@ -0,0 +1,14 @@
+//Name: shravani rajkumar gholap
+//PRN:-2503033111372032
+#include<stdio.h>
+int main(){
+      float D,r;
+      printf("Enter diameter: ");
+      scanf("%f",&D);
+      r=D/2;
+//Area of circle
+      printf("Area of circle is %f\n",3.14*r*r);
+//circumference of circle
+      printf("circumference of circle is %f: ",2*3.14*r);   
+      return 0;
+}    
