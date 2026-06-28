e40a1df04013f0557011c4166dea9316d132fe6b
shravanigholap021-max <shravanigholap021@gmail.com>
2026-06-28 18:19:22 +0530

    Add files via upload

---
+++ Area perimeter of rectangle. C.cpp
@@ -0,0 +1,16 @@
+//Name: shravani rajkumar gholap
+//PRN:-2503033111372032
+#include<stdio.h>
+int main(){
+      int a,b;
+      printf("Enter Length: ");
+      scanf("%d",&a);
+
+       printf("Enter Breadth: ");
+      scanf("%d",&b);
+//Area of Rectangle 
+      printf("Area of rectangle: %d\n",a*b);
+//perimeter OF rectangle 
+      printf("Perimeter of Rectangle: %d", 2*(a+b));   
+      return 0;
+}    
