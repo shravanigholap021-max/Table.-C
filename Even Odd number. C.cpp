8d4642547ad87eb836b6382144c6aa0cd8e81c7a
shravanigholap021-max <shravanigholap021@gmail.com>
2026-06-29 17:34:36 +0530

    Add files via upload

---
+++ Even Odd number. C.cpp
@@ -0,0 +1,16 @@
+//Name: shravani rajkumar gholap
+//PRN:-2503033111372032
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter A Number: ");
+    scanf("%d",&num);
+//check whether the number is even or odd
+    if(num%2 == 0){
+        printf("Number is Even"); 
+    }
+    else{
+        printf("Number is Odd");
+    }
+    return 0;
+}
