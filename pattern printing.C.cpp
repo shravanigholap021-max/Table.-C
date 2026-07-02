1909ddbf23d23195836f6e18d720faaf56d669bb
shravanigholap021-max <shravanigholap021@gmail.com>
2026-07-02 17:24:28 +0530

    Add files via upload

---
+++ pattern printing.C.cpp
@@ -0,0 +1,12 @@
+//Name: shravani rajkumar gholap
+//PRN:-2503033111372032
+ #include <stdio.h>
+ int main(){
+     int i,j;
+     for(i=1;i<4;i++) {
+        for(j=1;j<4;j++){
+           printf("*\t");
+        }
+        printf("\n");
+     }
+ }    
