79e079275890a836941ad9e25b79213b7381a000
shravanigholap021-max <shravanigholap021@gmail.com>
2026-07-09 16:13:24 +0530

    Add files via upload

---
+++ Hello world using printf scanf. C.cpp
@@ -0,0 +1,14 @@
+//Name: shravani rajkumar gholap
+//PRN:-2503033111372032
+#include<stdio.h>
+int main()
+{
+    int num1, num2, sum;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    sum=num1+num2;
+    printf("The sum of %d and %d is %d", num1,num2, sum);
+    return 0;
+}    
