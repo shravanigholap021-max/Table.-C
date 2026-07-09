712a9123497157b214ce05645d781464465ed10b
shravanigholap021-max <shravanigholap021@gmail.com>
2026-07-09 15:19:05 +0530

    Add files via upload

---
+++ lower triangular. C.cpp
@@ -0,0 +1,12 @@
+//Name: shravani rajkumar gholap
+//PRN:-2503033111372032
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=0;i<3;i++){
+        for(j=0;j<3-i;j++){
+            printf("*\t");
+        }
+        printf("\n");
+    }
+}    
