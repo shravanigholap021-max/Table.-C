84524193c64770308938bbbddf075461fd957cee
shravanigholap021-max <shravanigholap021@gmail.com>
2026-07-09 15:36:19 +0530

    Add files via upload

---
+++ Iterations. C.cpp
@@ -0,0 +1,14 @@
+//Name: shravani rajkumar gholap
+//PRN:-2503033111372032
+#include <stdio.h>
+int main() {
+    int i;
+    for (i = 1; i <= 5; i++) {
+    if (i == 3) {
+       continue; 
+   }
+   printf("\n%d", i);
+}
+return 0;
+}
+    
